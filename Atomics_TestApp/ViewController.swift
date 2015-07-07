4a1614c3e8b66d694184cbe817cdbb866dc10622
Guillaume Lessard <dhtnstff@gmail.com>
2015-07-07 16:02:46 -0600

    An iOS test app target.
    Doesn't allow running tests on the embedded framework at the moment.

---
+++ Atomics_TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Atomics_TestApp
+//
+//  Created by Guillaume Lessard on 2015-07-07.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
