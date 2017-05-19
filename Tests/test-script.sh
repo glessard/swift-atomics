0cb836c48a3810518eced3d79ec96d820d99ea70
Guillaume Lessard <dhtnstff@gmail.com>
2017-05-19 15:03:23 -0600

    test that gyb files are in sync with their output files

--- Tests/test-script.sh
+++ Tests/test-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-cd "$(dirname $0)/../"
-
 swift test -s ClangAtomicsTests.ClangAtomicsTests
 swift test -s AtomicsTests.MemoryOrderTests
 swift test -s AtomicsTests.AtomicsTests
