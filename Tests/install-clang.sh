4258db6802fd6b88911d47dd8c30d48e38db631c
Guillaume Lessard <dhtnstff@gmail.com>
2017-09-20 10:39:34 -0600

    update clang handling for updated linux environment on travis-ci

--- Tests/install-clang.sh
+++ Tests/install-clang.sh
@@ -3,20 +3,17 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
-  if [[ -n "$CLANG_VERSION" ]]
+  if [[ -z "$CLANG_VERSION" ]]
   then
-    # remove outdated clang
-    /bin/rm -rf /usr/local/clang-3.5.0
-    /bin/rm -f  /usr/local/clang
-
-    # install desired clang
-    apt-get update -qq
-    apt-get install -qq -y clang-${CLANG_VERSION}
-    update-alternatives --quiet --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
-    update-alternatives --quiet --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
-
-    echo ""
+    CLANG_VERSION=3.9
   fi
+  # install desired clang
+  apt-get update -qq
+  apt-get install -qq -y clang-${CLANG_VERSION}
+  update-alternatives --quiet --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
+  update-alternatives --quiet --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
+
+  echo ""
 fi
 
 clang --version
