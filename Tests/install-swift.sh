219e08ffec9ca7f2b288cded648542883348ca27
Guillaume Lessard <dhtnstff@gmail.com>
2017-12-05 15:46:12 -0700

    update CI configuration

--- Tests/install-swift.sh
+++ Tests/install-swift.sh
@@ -3,14 +3,22 @@ set -e
 
 if [[ -n "$SWIFT_VERSION" ]]
 then
+  export SWIFT_TOOLS_VERSION=`echo $SWIFT_VERSION | awk -F . '{print $1"."$2}'`
+  if [[ "$SWIFT_TOOLS_VERSION" == "3.2" ]]
+  then
+    SWIFT_PACKAGE_VERSION=`echo $SWIFT_VERSION | awk -F . -v ver=4.0 '{if(NF==2) print ver; else print ver"."$3}'`
+  else
+    SWIFT_PACKAGE_VERSION=$SWIFT_VERSION
+  fi
+
   if [[ -n "$SNAPSHOT" ]]
   then
     ID="DEVELOPMENT-SNAPSHOT-${SNAPSHOT}"
-    VERSION="swift-${SWIFT_VERSION}-${ID}"
-    BRANCH="swift-${SWIFT_VERSION}-branch"
+    VERSION="swift-${SWIFT_PACKAGE_VERSION}-${ID}"
+    BRANCH="swift-${SWIFT_PACKAGE_VERSION}-branch"
   else
-    VERSION="swift-${SWIFT_VERSION}-RELEASE"
-    BRANCH="swift-${SWIFT_VERSION}-release"
+    VERSION="swift-${SWIFT_PACKAGE_VERSION}-RELEASE"
+    BRANCH="swift-${SWIFT_PACKAGE_VERSION}-release"
   fi
   URLBASE="https://swift.org/builds/${BRANCH}"
 
@@ -21,12 +29,10 @@ then
 
     # install swift
     cd ..
-    wget --no-verbose ${URLBASE}/${PLATFORM}/${VERSION}/${BASENAME}.tar.gz
+    curl -s -O ${URLBASE}/${PLATFORM}/${VERSION}/${BASENAME}.tar.gz
     tar xzf ${BASENAME}.tar.gz
     export PATH="${PWD}/${BASENAME}/usr/bin:${PATH}"
     cd "${TRAVIS_BUILD_DIR}"
-
-    echo ""
   elif [[ "$TRAVIS_OS_NAME" == "osx" && -n "$SNAPSHOT" ]]
   then
     PLATFORM="xcode"
@@ -37,12 +43,10 @@ then
     sudo installer -pkg ${BASENAME}.pkg -target /
     export TOOLCHAINS=swift
   fi
-else
-  if [[ -z $(which swift) ]]
-  then
-    echo "Set SWIFT_VERSION to define which version to install"
-    exit 1
-  fi
+elif [[ -z $(which swift) ]]
+then
+  echo "Set SWIFT_VERSION to define which version to install"
+  exit 1
 fi
 
 swift --version
--- Tests/test-script.sh
+++ Tests/test-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-if [[ "${TRAVIS_OS_NAME}" == "osx" && -n "${SWIFT_VERSION}" ]]
+if [[ -n "${SWIFT_TOOLS_VERSION}" ]]
 then
-  swift package tools-version --set "${SWIFT_VERSION}"
+  swift package tools-version --set "${SWIFT_TOOLS_VERSION}"
 fi
 
 swift test
