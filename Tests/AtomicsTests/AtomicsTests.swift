29d2f5a5923376cac86db74456e3098f67905264
Guillaume Lessard <dhtnstff@gmail.com>
2018-06-14 23:38:19 -0600

    in tests, use the stdlib's random number generator when it exists

--- Tests/AtomicsTests/AtomicsTests.swift
+++ Tests/AtomicsTests/AtomicsTests.swift
@@ -9,45 +9,51 @@
 import XCTest
 import Dispatch
 
+#if !swift(>=4.2)
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 import func Darwin.C.stdlib.arc4random
 #else // assuming os(Linux)
 import func Glibc.random
 #endif
+#endif
 
 import Atomics
 
 #if swift(>=4.0)
-extension FixedWidthInteger
+extension FixedWidthInteger where Self.Magnitude: UnsignedInteger, Self.Stride: SignedInteger
 {
   // returns a positive random integer greater than 0 and less-than-or-equal to Self.max/2
-  // the least significant bit is always set.
   static func nzRandom() -> Self
   {
+#if swift(>=4.2)
+    return Self.random(in: 1...(Self.max>>1))
+#else
     var t = Self()
     for _ in 0...((t.bitWidth-1)/32)
     {
-    #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
       t = t<<32 &+ Self(truncatingIfNeeded: arc4random())
-    #else // probably Linux
+#else // probably Linux
       t = t<<32 &+ Self(truncatingIfNeeded: random())
-    #endif
+#endif
     }
+    // in this variant the least significant bit is always set.
     return (t|1) & (Self.max>>1)
+#endif
   }
 }
 #else
 extension UInt
 {
   // returns a positive random integer greater than 0 and less-than-or-equal to UInt32.max/2
-  // the least significant bit is always set.
+  // in this variant the least significant bit is always set.
   static func nzRandom() -> UInt
   {
-  #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
     return UInt(arc4random() & 0x3fff_fffe + 1)
-  #else
+#else
     return UInt(random() & 0x3fff_fffe + 1)
-  #endif
+#endif
   }
 }
 #endif
