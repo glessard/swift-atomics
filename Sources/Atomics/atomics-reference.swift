1fe24a16f6b4ac8f2701203956b9ca5caf1234d4
Guillaume Lessard <dhtnstff@gmail.com>
2018-12-05 09:51:03 -0700

    make compatible with Swift 3.1.1 on Linux

--- Sources/Atomics/atomics-reference.swift
+++ Sources/Atomics/atomics-reference.swift
@@ -13,6 +13,17 @@
 
 import struct CAtomics.OpaqueUnmanagedHelper
 
+#if swift(>=3.2)
+#else
+extension MemoryOrder
+{
+  @_versioned init(loadOrder: LoadMemoryOrder)
+  {
+    self = MemoryOrder.init(rawValue: loadOrder.rawValue) ?? .sequential
+  }
+}
+#endif
+
 public struct AtomicReference<T: AnyObject>
 {
 #if swift(>=4.2)
@@ -96,11 +107,18 @@ extension AtomicReference
     let pointer = ptr.spinSwap(nil, MemoryOrder(rawValue: order.rawValue)!)
     return pointer.map(Unmanaged.fromOpaque)?.takeRetainedValue()
   }
-#else
+#elseif swift(>=3.2)
   @inline(__always)
   public mutating func take(order: LoadMemoryOrder = .sequential) -> T?
   {
     let pointer = ptr.spinSwap(nil, MemoryOrder(rawValue: order.rawValue)!)
+    return pointer.map(Unmanaged.fromOpaque)?.takeRetainedValue()
+  }
+#else // swift 3.1
+  @inline(__always)
+  public mutating func take(order: LoadMemoryOrder = .sequential) -> T?
+  {
+    let pointer = ptr.spinSwap(nil, MemoryOrder(loadOrder: order))
     return pointer.map(Unmanaged.fromOpaque)?.takeRetainedValue()
   }
 #endif
