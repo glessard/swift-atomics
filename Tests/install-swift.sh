bdea7024acc3291b4dffc031eb7c90addb9edbd7
Guillaume Lessard <dhtnstff@gmail.com>
2019-02-15 12:02:29 -0700

    update installation and test scripts

--- Tests/install-swift.sh
+++ Tests/install-swift.sh
@@ -3,58 +3,27 @@ set -e
 
 if [[ -n "$SWIFT" ]]
 then
-  if [[ "$SWIFT" == "3.2" ]]
-  then
-    export SWIFT_TOOLS_VERSION="3.1"
-    export SWIFT_COMPILER_VERSION="4.0"
-  elif [[ "$SWIFT" == "3.3" ]]
-  then
-    export SWIFT_TOOLS_VERSION="3.1"
-    export SWIFT_COMPILER_VERSION="4.2"
-  elif [[ "$SWIFT" == "4.1.50" ]]
-  then
-    export SWIFT_TOOLS_VERSION="4.0"
-    export SWIFT_COMPILER_VERSION="4.2"
-  else
-    export SWIFT_TOOLS_VERSION=`echo $SWIFT | awk -F . '{print $1"."$2}'`
-    export SWIFT_COMPILER_VERSION=$SWIFT
-  fi
+  export COMPILER_VERSION=$SWIFT
 
-  if [[ -n "$SNAPSHOT" ]]
-  then
-    ID="DEVELOPMENT-SNAPSHOT-${SNAPSHOT}"
-    VERSION="swift-${SWIFT_COMPILER_VERSION}-${ID}"
-    BRANCH="swift-${SWIFT_COMPILER_VERSION}-branch"
-  else
-    VERSION="swift-${SWIFT_COMPILER_VERSION}-RELEASE"
-    BRANCH="swift-${SWIFT_COMPILER_VERSION}-release"
-  fi
+  COMPILER="swift-${COMPILER_VERSION}-RELEASE"
+  BRANCH="swift-${COMPILER_VERSION}-release"
   URLBASE="https://swift.org/builds/${BRANCH}"
 
   if [[ "$TRAVIS_OS_NAME" == "linux" ]]
   then
     PLATFORM="ubuntu1604"
-    BASENAME="${VERSION}-ubuntu16.04"
+    BASENAME="${COMPILER}-ubuntu16.04"
 
     # install swift
     cd ..
-    curl -s -O ${URLBASE}/${PLATFORM}/${VERSION}/${BASENAME}.tar.gz
+    curl -s -O ${URLBASE}/${PLATFORM}/${COMPILER}/${BASENAME}.tar.gz
     tar xzf ${BASENAME}.tar.gz
     export PATH="${PWD}/${BASENAME}/usr/bin:${PATH}"
     cd "${TRAVIS_BUILD_DIR}"
-  elif [[ "$TRAVIS_OS_NAME" == "osx" && -n "$SNAPSHOT" ]]
-  then
-    PLATFORM="xcode"
-    BASENAME="${VERSION}-osx"
-
-    # install swift
-    curl -s -O ${URLBASE}/${PLATFORM}/${VERSION}/${BASENAME}.pkg
-    sudo installer -pkg ${BASENAME}.pkg -target /
-    export TOOLCHAINS=swift
   fi
 elif [[ -z $(which swift) ]]
 then
-  echo "Set SWIFT to define which version to install"
+  echo "Set SWIFT to define which compiler version to install"
   exit 1
 fi
 
