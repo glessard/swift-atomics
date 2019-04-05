e108fabf99be16a57f8ec771df5295610ee46de2
Guillaume Lessard <dhtnstff@gmail.com>
2019-04-05 11:15:06 -0600

    improve random numbers used in tests again

--- Tests/AtomicsTests/RandomPositive.swift
+++ Tests/AtomicsTests/RandomPositive.swift
@@ -2,12 +2,21 @@
 //  RandomPositive.swift
 //
 
-#if !swift(>=4.2)
+#if swift(>=4.1.50)
+extension FixedWidthInteger where Self.Magnitude: UnsignedInteger, Self.Stride: SignedInteger
+{
+  // returns a positive random integer greater than 0 and less-than-or-equal to Self.max/2
+  static func randomPositive() -> Self
+  {
+    return Self.random(in: 1...(Self.max>>1))
+  }
+}
+#else
+
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 import func Darwin.C.stdlib.arc4random
 #else // assuming os(Linux)
 import func Glibc.random
-#endif
 #endif
 
 #if swift(>=4.0)
@@ -16,43 +25,39 @@ extension FixedWidthInteger where Self.Magnitude: UnsignedInteger, Self.Stride: 
   // returns a positive random integer greater than 0 and less-than-or-equal to Self.max/2
   static func randomPositive() -> Self
   {
-#if swift(>=4.1.50)
-    return Self.random(in: 1...(Self.max>>1))
-#else
-    var t = Self()
+    var t: Self = 0
     repeat {
       for _ in 0...((t.bitWidth-1)/32)
       {
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
         t = t<<32 &+ Self(truncatingIfNeeded: arc4random())
 #else // probably Linux
-        t = t<<32 &+ Self(truncatingIfNeeded: random())
+        t = t<<32 &+ Self(truncatingIfNeeded: Glibc.random())
 #endif
       }
-    } while t == Self()
+    } while t == 0
 
     return t & (Self.max>>1)
-#endif
   }
 }
 #else
 extension UInt
 {
   // returns a positive random integer greater than 0 and less-than-or-equal to UInt32.max/2
-  // in this variant the least significant bit is always set.
   static func randomPositive() -> UInt
   {
     var t: UInt
     repeat {
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
       t = UInt(arc4random())
-#else
+#else // probably Linux
       t = UInt(Glibc.random())
 #endif
-      t &= 0x3fff_ffff
     } while t == 0
 
-    return t
+    return t & (UInt.max>>1)
   }
 }
-#endif
+#endif // swift(>=4.0)
+
+#endif // swift(>=4.1.50)
