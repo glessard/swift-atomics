d2ae57aa663064a0d81b170c6b0c5d2a02d45731
Guillaume Lessard <dhtnstff@gmail.com>
2020-12-21 10:46:53 -0700

    support only Swift 3.1 on this branch

--- Package.swift
+++ Package.swift
@@ -1,25 +1,6 @@
-// swift-tools-version:4.2
+// swift-tools-version:3.1.0
 
 import PackageDescription
-
-#if swift(>=4.0)
-
-let package = Package(
-  name: "SwiftAtomics",
-  products: [
-    .library(name: "SwiftAtomics", targets: ["SwiftAtomics"]),
-    .library(name: "CAtomics", targets: ["CAtomics"]),
-  ],
-  targets: [
-    .target(name: "SwiftAtomics", dependencies: ["CAtomics"]),
-    .testTarget(name: "SwiftAtomicsTests", dependencies: ["SwiftAtomics"]),
-    .target(name: "CAtomics", dependencies: []),
-    .testTarget(name: "CAtomicsTests", dependencies: ["CAtomics"]),
-  ],
-  swiftLanguageVersions: [.v3, .v4, .v4_2, .version("5")]
-)
-
-#else
 
 let package = Package(
   name: "SwiftAtomics",
@@ -27,5 +8,3 @@ let package = Package(
     Target(name: "SwiftAtomics", dependencies: ["CAtomics"]),
   ]
 )
-
-#endif
